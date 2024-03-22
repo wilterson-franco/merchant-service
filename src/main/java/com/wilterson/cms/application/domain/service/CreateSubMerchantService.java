c83f34cf89eaee4fd3d6784064c3d5e7035cf58e
Wilterson Franco <wilterson.franco@hotmail.com>
2024-03-22 19:36:30 -0400

    Syntatic Validation
    Moves the syntatic validation to the CreateSubMerchantService. The
    rational is that there will be use cases in the future that don't
    require syntatic validation.

--- src/main/java/com/wilterson/cms/application/domain/service/CreateSubMerchantService.java
+++ src/main/java/com/wilterson/cms/application/domain/service/CreateSubMerchantService.java
@@ -7,6 +7,7 @@ package com.wilterson.cms.application.domain.service;
 import com.wilterson.cms.application.domain.model.SubMerchant;
 import com.wilterson.cms.application.port.in.SubMerchantCommand;
 import com.wilterson.cms.common.StringGenerator;
+import com.wilterson.cms.common.validation.SyntacticValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,13 +16,22 @@ class CreateSubMerchantService {
 
     private static final int GUID_LENGTH = 16;
     private MerchantMapper merchantMapper;
+    private SyntacticValidator syntacticValidator;
 
     @Autowired
     final void setMerchantMapper(MerchantMapper merchantMapper) {
         this.merchantMapper = merchantMapper;
     }
 
+    @Autowired
+    public void setSyntacticValidator(SyntacticValidator syntacticValidator) {
+        this.syntacticValidator = syntacticValidator;
+    }
+
     SubMerchant createSubMerchant(SubMerchantCommand subMerchantCommand) {
+
+        syntacticValidator.validate(subMerchantCommand);
+
         return merchantMapper.toDomainEntity(subMerchantCommand, StringGenerator.generate(GUID_LENGTH));
     }
 }
--- src/test/java/com/wilterson/cms/application/domain/service/CreateSubMerchantServiceTest.java
+++ src/test/java/com/wilterson/cms/application/domain/service/CreateSubMerchantServiceTest.java
@@ -10,6 +10,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import com.wilterson.cms.application.port.in.SubMerchantCommand;
+import com.wilterson.cms.common.validation.SyntacticValidator;
 import java.util.Collections;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,13 +22,16 @@ import org.mockito.junit.jupiter.MockitoExtension;
 class CreateSubMerchantServiceTest {
 
     @Mock
+    private SyntacticValidator syntacticValidator;
+
+    @Mock
     MerchantMapper merchantMapper;
 
     @InjectMocks
     private CreateSubMerchantService createSubMerchantService;
 
     @Test
-    void whenCreateSubMerchant_thenValidationShouldKickIn() {
+    void whenCreateSubMerchant_thenValidatorAndMapperShouldBeCalled() {
 
         // given
         var command = new SubMerchantCommand("MERCHANT-NAME", Collections.emptyList());
@@ -36,6 +40,7 @@ class CreateSubMerchantServiceTest {
         createSubMerchantService.createSubMerchant(command);
 
         // then
+        verify(syntacticValidator, times(1)).validate(command);
         verify(merchantMapper, times(1)).toDomainEntity(eq(command), anyString());
     }
 }
